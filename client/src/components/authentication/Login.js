c01774c6901a89ff0ae2b3f18aafb421b4347ca8
sankalp <sankalp25103@gmail.com>
2023-07-16 21:28:12 +0530

    login

--- client/src/components/authentication/Login.js
+++ client/src/components/authentication/Login.js
@@ -4,61 +4,85 @@ import classes from "./Login.module.css";
 import Card from "../UI/Card";
 
 const Login = (props) => {
-    const [istouched, setIsTouched]=useState(false);
-    const [validUserId, isUserIdValid]=useState(false);
-    const [validPassword, setValidPassword]=useState(true);
-    const [userid, setUserId]=useState('');
-    const [password, setPassowrd]=useState('');
-    const changeUserIdhandler=(event)=>{
-        console.log(event.target.value);
-        setUserId(event.target.value);
+  const [istouched, setIsTouched] = useState(false);
+  const [validUserId, isUserIdValid] = useState(false);
+  const [validPassword, setValidPassword] = useState(true);
+  const [userid, setUserId] = useState("");
+  const [password, setPassowrd] = useState("");
+
+  const changeUserIdhandler = (event) => {
+    console.log(event.target.value);
+    setUserId(event.target.value);
+  };
+
+  const useridInputBlueHandler = () => {
+    setIsTouched(true);
+    console.log("here");
+    if (userid.length < 6) {
+      console.log("here1");
+      isUserIdValid(false);
+      return;
     }
-    const useridInputBlueHandler=()=>{
-        setIsTouched(true);
-        console.log("here");
-        if(userid.length<6)
-        {
-            console.log("here1");
-            isUserIdValid(false);
-            return;
-        }   
-        isUserIdValid(true);
+    isUserIdValid(true);
+  };
+
+  const changePasswordHandler = (event) => {
+    setPassowrd(event.target.value);
+  };
+
+  const passwordCheckHandler = () => {
+    if (password.trim() === "") {
+      setValidPassword(false);
+      return;
     }
-    const changePasswordHandler=(event)=>{
-        setPassowrd(event.target.value);
-    }
-    const passwordCheckHandler=()=>{
-        if(password.trim()==='')
-        {
-            setValidPassword(false);
-            return;
-        }
-        setValidPassword(true);
-    }
-    const FormSubmitHandler=(event)=>
-    {
-        event.preventDefault();
-        setIsTouched(true);
-    }
-    const isValidClass=istouched && !validUserId;
-    const firstNameClasses = isValidClass ? 'form-control invalid' : 'form-control';
-    console.log(isValidClass);
-    const passwordClass=validPassword ? "form-control" : "form-control invalid"
+    setValidPassword(true);
+  };
+
+  const FormSubmitHandler = (event) => {
+    event.preventDefault();
+    setIsTouched(true);
+  };
+
+  const isValidClass = istouched && !validUserId;
+
+  const firstNameClasses = isValidClass
+    ? "form-control invalid"
+    : "form-control";
+  console.log(isValidClass);
+
+  const passwordClass = validPassword ? "form-control" : "form-control invalid";
+
   return (
     <div className={classes.container}>
       <Card>
         <div className={classes["login-form"]}>
           <h1 className={classes["login-heading"]}>Login</h1>
-          <form onSubmit={FormSubmitHandler}> 
+          <form onSubmit={FormSubmitHandler}>
             <label>Enter Your User Id</label>
             <br></br>
-            <input className={firstNameClasses} name="userid" onBlur={useridInputBlueHandler} onChange={changeUserIdhandler} type="text" id="userid"></input>
+            <input
+              className={firstNameClasses}
+              name="userid"
+              onBlur={useridInputBlueHandler}
+              onChange={changeUserIdhandler}
+              type="text"
+              id="userid"
+            ></input>
             <br></br>
             <label>Enter Your Password</label>
             <br></br>
-            <input name="password" className={passwordClass} onChange={changePasswordHandler} onBlur={passwordCheckHandler}  type="password" id="password"></input>
+            <input
+              name="password"
+              className={passwordClass}
+              onChange={changePasswordHandler}
+              onBlur={passwordCheckHandler}
+              type="password"
+              id="password"
+            ></input>
           </form>
-          {isValidClass && <p className="error-text">Please enter a Valid userid.</p>}
+          {isValidClass && (
+            <p className="error-text">Please enter a Valid userid.</p>
+          )}
           {!validPassword && <p>Enter Password</p>}
           <button>Login</button>
           <Link to="/signup">
--- client/src/components/authentication/Signup.js
+++ client/src/components/authentication/Signup.js
@@ -1,32 +1,72 @@
-
 import { Link } from "react-router-dom";
 import Card from "../UI/Card";
 import classes from "./Signup.module.css";
+import React, {useState} from "react";
 const Signup = (props) => {
+
+  const [isUserIdTouched, setUserIdTouched]=useState(false);
+  const [isPasswordTouched, setPasswordTouched]=useState(false);
+  const [isConfirmPasswordTouched,, setConfirmPasswordValid]=useState(false);
+  const [isUserIdValid, setUsedIdValid]=useState(false);
+  const [isPasswordValid, setPasswordValid]=useState(false);
+  const [UserId, setUserId]=useState('');
+  const [password, setPassword]=useState('');
+  const [confirmPassword, setConfirmPassword]=('');
+
+  const userIdTouchedHandler=()=>{
+    setUsedIdValid(true);
+  }
+
+  const passwordTouchedHandler=()=>{
+      setPasswordValid(true);
+  }
+
+  const confirmPasswordHandler=()=>{
+    setConfirmPasswordValid(true);
+  }
+
+  const changeUserIdHandler=(event)=>{
+    console.log(event.target.value);
+    setUserId(event.target.value);
+  }
+
+  const changePasswordHandler=(event)=>{
+    console.log(event.target.value);
+    setPassword(event.target.value);
+  }
+
+  const changeConfirmPasswordHandler=(event)=>{
+    console.log(event.target.value);
+    setConfirmPassword(event.target.value);
+  }
+
+  const formSubmitHandler=(event)=>{
+    setUserIdTouched(true);
+    setPasswordTouched(true);
+    setConfirmPasswordValid(true);
+    event.preventDefault(false);
+  }
+
   return (
     <>
       <div className={classes.container}>
         <Card>
           <div className={classes["login-form"]}>
             <h1 className={classes["login-heading"]}>Signup</h1>
-            <form>
-              <label>Enter Your Name</label>
+            <form onSubmit={formSubmitHandler}>
+              <label>Enter Your UserId</label>
               <br></br>
-              <input name="name" type="text" id="name"></input>
-              <br></br>
-              <label>Enter Your Email ID</label>
-              <br></br>
-              <input name="email" type="email" id="email"></input>
-              <br></br>
+              <input name="name" onBlur={userIdTouchedHandler} onChange={changeUserIdHandler} type="text" id="name"></input>
               <label>Enter Your Password</label>
               <br></br>
-              <input name="password" type="password" id="password"></input>
+              <input name="password" onBlur={passwordTouchedHandler} onChange={changePasswordHandler} type="password" id="password"></input>
               <label>Confirm Your Password</label>
               <br></br>
-              <input name="password" type="password" id="confirmPasword"></input>
+              <input name="confirmpassword" onBlur={confirmPasswordHandler} onChange={changeConfirmPasswordHandler} type="password" id="confirmPasword"></input>
               <br></br>
+              <button type='submit'>Signup</button>
             </form>
-            <button>Login</button>
+            
             <Link to="/login">Already Register? Click here to login</Link>
           </div>
         </Card>
